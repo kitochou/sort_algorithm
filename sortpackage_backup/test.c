155c9a88f079aa96bb38c13d03270efe9f0b696d
kitochou <kitochou@gmail.com>
2013-01-03 22:04:33 +0800

    as a project

--- sortpackage_backup/test.c
+++ sortpackage_backup/test.c
@@ -1,3 +1,8 @@
+//if U want to try other sort algorithms, U need to do:
+//1. add the functions respectively;
+//2. rewrite the Makefile, but U just need to add something, it is trivial
+//3. extern variable is alse need to add to the test.c
+
 #include "sort.h"
 
 extern int  array[SCALE] = {3,7,2,5,1};
